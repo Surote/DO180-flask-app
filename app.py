b58f1c78129a42bb4b9905bb364a038fa082e4e9
Surote <surote.z9@gmail.com>
2022-09-22 12:59:13 +0700

    Create app.py
    init

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import requests
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    req=requests.get('http://worldclockapi.com/api/json/est/now')
+    data=json.loads(req.text)
+    return data
