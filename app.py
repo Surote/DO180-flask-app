7471088e89fb957506cb14649dc857e2c953ea7e
Surote Wongpaiboon <surote.z9@gmail.com>
2022-11-04 13:42:35 +0700

    fix: test2

--- app.py
+++ app.py
@@ -9,5 +9,6 @@ def hello_world():
     req=requests.get('http://worldclockapi.com/api/json/est/now')
     data=json.loads(req.text)
     return data
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
