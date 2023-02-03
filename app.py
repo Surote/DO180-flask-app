b4e357d11612f5eff072330de5717608bb6686fe
Surote Wongpaiboon <surote.z9@gmail.com>
2023-02-03 12:19:59 +0700

    fix: API # 5

--- app.py
+++ app.py
@@ -7,18 +7,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    req_here=os.getenv("API","API2")
-    req=requests.get(req_here)
-    if req.status_code != 200:
-        print('API 1 not works!!')
-        req_here2=os.getenv("API2","API")
-        req2=requests.get(req_here2)
-        data=json.loads(req2.text)
+    try:
+        req_here=os.getenv("API","API2")
+        req=requests.get(req_here)
+        data=json.loads(req.text)
         return data
-    else:
+    except:
+        req_here=os.getenv("API2","API")
+        req=requests.get(req_here)
         data=json.loads(req.text)
         return data
 
     
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',port=1234)
